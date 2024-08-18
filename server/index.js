8f5989f534d2de45e268e79334ae60ee68e46855
Siddhartha <1905908@kiit.ac.in>
2024-08-19 00:36:41 +0530

    database configured to the project

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
 const app = express();
 app.listen(3000, () => {
   console.log(
