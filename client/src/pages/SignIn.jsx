c3f5cf6578d6d2bb6b3bb5b19334464eda594f63
Siddhartha <1905908@kiit.ac.in>
2024-08-20 02:24:19 +0530

    redux toolkit added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaLaptopHouse } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  // Below is the code for the redux store without using usestate
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -12,8 +23,8 @@ export default function SignIn() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent the form from refreshing the page
-    setLoading(true); // Start the loading state
     try {
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -23,16 +34,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.sucess == false) {
-        setError(data.message), setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       console.log(data);
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
+      console.log(error);
     }
   };
   return (
