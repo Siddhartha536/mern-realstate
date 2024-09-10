5810d89c152cf934db1d8ed094039fba8db1b9a8
Siddhartha <1905908@kiit.ac.in>
2024-09-10 15:55:39 +0530

    create listing ui added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import { app } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import {
   getStorage,
@@ -213,6 +213,9 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+        Create Listing
+        </Link>
       </form>
       <div className=" flex justify-between mt-5">
         <span
